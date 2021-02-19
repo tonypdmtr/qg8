9148a21e31cf608b0f74d5cba1627a84e7b83448
Tony Papadimitriou <tonypdmtr@users.noreply.github.com>
2021-02-19 15:18:16 +0200

    Added preliminary [WIP] derivative.h needed by rom.c

---
+++ derivative.h
@@ -0,0 +1,18 @@
+typedef unsigned int uint16_t;
+#define PTADD (*(volatile uint16_t*)0x0001)
+#define PTAD_PTAD0 0x0000
+#define PTAD_PTAD1 0x0001
+#define PTAD_PTAD2 0x0002
+#define PTAD_PTAD3 0x0003
+#define PTASE (*(volatile uint16_t*)0x1841)
+#define PTBDD (*(volatile uint16_t*)0x0003)
+#define PTBD_PTBD0 0x0000
+#define PTBD_PTBD1 0x0001
+#define PTBD_PTBD2 0x0002
+#define PTBD_PTBD3 0x0003
+#define PTBD_PTBD4 0x0004
+#define PTBD_PTBD5 0x0005
+#define PTBD_PTBD6 0x0006
+#define PTBD_PTBD7 0x0007
+#define PTBSE (*(volatile uint16_t*)0x1845)
+#define SOPT1 (*(volatile uint16_t*)0x1802)
